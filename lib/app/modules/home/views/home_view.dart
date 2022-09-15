b38feac3b8b2d7f624c9cfbb9ed4851cc17c7643
RandhoB <randho.bagas12@gmail.com>
2022-09-15 23:16:53 +0700

    perubahan lanjutan

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'package:get/get.dart';
 import 'package:helloworld_alkademi/theme.dart';
@@ -11,41 +12,80 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 1,
+        backgroundColor: appColor,
         leading: IconButton(
-          icon: Icon(Icons.menu),
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black45,
+          ),
           onPressed: () {},
         ),
         title: Text(
           'Hello Alkademi',
-          style: appStyle.copyWith(),
+          style: appStyle.copyWith(
+            color: Colors.black45,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         centerTitle: false,
         actions: [
-          Icon(Icons.ac_unit),
+          Container(
+              child: Icon(
+            Icons.ac_unit,
+            color: Colors.black45,
+          )),
+          SizedBox(
+            width: 30,
+          ),
+          Container(
+              child: Icon(
+            Icons.abc_rounded,
+            color: Colors.black45,
+          )),
           SizedBox(
             width: 10,
           ),
-          Icon(Icons.abc_rounded),
         ],
       ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Hello',
-              style: bodyJudulStyle.copyWith(),
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/background.jpeg',
+              fit: BoxFit.cover,
+              height: Get.height,
+              width: Get.width,
             ),
-            SizedBox(
-              width: Get.width * 0.5,
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset('assets/landmark.svg'),
+                Text(
+                  'Hello',
+                  style: bodyJudulStyle.copyWith(
+                    fontSize: 40,
+                    color: youngBlue,
+                  ),
+                ),
+                SizedBox(
+                  width: 100,
+                ),
+                Text(
+                  'World',
+                  style: bodyIsiStyle.copyWith(
+                    fontSize: 60,
+                    color: youngRed,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'World',
-              style: bodyIsiStyle.copyWith(),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
